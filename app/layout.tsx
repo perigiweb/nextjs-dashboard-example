a936bf28e192b1babc4bb856d7bed350974076a9
Perigi Web <admin@perigi.my.id>
2025-07-01 06:24:36 +0700

    add @vercel/analytics and link to github repo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
-
 import Link from 'next/link'
+import { Analytics } from '@vercel/analytics/next'
 
 import '@/style/global.css';
 import { inter, jost } from './font'
@@ -21,8 +21,9 @@ export default function RootLayout({
 		    <header className="px-4 py-3 bg-slate-300 dark:bg-slate-700"><Link className="font-jost font-bold text-lg" href="/">{metadata.title as string}</Link></header>
 		    <main className="w-full px-4 py-6 md:py-8">{children}</main>
         <footer className="border-t border-slate-400 dark:border-slate-600 p-4">
-          <p className="text-center">&copy; 2025 by <a href="https://perigi.my.id" target="_blank" className="underline decoration-orange-600 hover:text-orange-700/90">Perigi Web</a></p>
+          <p className="text-center">&copy; 2025 by <a href="https://perigi.my.id" target="_blank" className="underline hover:text-orange-700/90">Perigi Web</a>. <a href="https://github.com/perigiweb/nextjs-dashboard-example" target="_blank" className="underline hover:text-orange-700/90">View Source Code</a></p>
         </footer>
+        <Analytics />
 	    </body>
     </html>
   )
