4441bbf8a8e4e58b9309436c4ee14148eb362b68
Perigi Web <admin@perigi.my.id>
2025-12-25 11:24:33 +0700

    update dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <body className={`font-sans bg-slate-200 dark:bg-slate-800 text-slate-900 dark:text-slate-100 ${inter.className}`}>
 		    <header className="px-4 py-3 bg-slate-300 dark:bg-slate-700"><Link className="font-jost font-bold text-lg" href="/">{metadata.title as string}</Link></header>
 		    <main className="w-full px-4 py-6 md:py-8">{children}</main>
-        <footer className="border-t border-slate-400 dark:border-slate-600 p-4">
+        <footer className="border-t border-slate-400 dark:border-slate-600 p-4 text-sm">
           <p className="text-center">&copy; 2025 by <a href="https://perigi.my.id" target="_blank" className="underline hover:text-orange-700/90">Perigi Web</a>. <a href="https://github.com/perigiweb/nextjs-dashboard-example" target="_blank" className="underline hover:text-orange-700/90">View Source Code</a></p>
         </footer>
         <Analytics />
